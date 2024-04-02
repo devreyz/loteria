e8e5158c0712e78263fa8e6c7fe0247669145866
reyzin <devreyz137@gmail.com>
2024-04-01 22:38:13 -0300

    first

---
+++ src/main/java/br/com/caelum/vraptor/controller/DashboardController.java
@@ -0,0 +1,24 @@
+package br.com.caelum.vraptor.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.model.Usuario;
+
+@Path("dashboard")
+@Controller
+public class DashboardController {
+	
+	@Get("")
+	public void dashboard() {
+		
+	}
+	
+	@Post("salvausuario")
+	public void salvausuario(Usuario usuario) {
+		
+		
+	}
+
+}
